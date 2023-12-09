16027be92f39f64c290f48a3e30f6eb99c21c383
jakingtosch <754734+Jakingtosch@users.noreply.github.com>
2023-12-09 21:43:06 +0100

    feat(1): Add part 2 of aoc day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(1);
 
-use regex::Regex;
+use regex::{Captures, Regex};
 
 pub fn part_one(input: &str) -> Option<u32> {
     let re = Regex::new(r"(\d{1})").unwrap();
@@ -24,7 +24,40 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let preprocess_re = Regex::new("(zero|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let preprocessed_reult = preprocess_re.replace_all(input, |cap: &Captures| {
+        match &cap[0] {
+            "zero" => "0",
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => panic!("We should never have gotten here!"),
+        }.to_string()
+    });
+    let re = Regex::new(r"(\d{1})").unwrap();
+    let numbers_collected: Vec<u32> = preprocessed_reult.lines().map(
+        |x| {
+            let numbers: Vec<&str> = re.find_iter(x).map(|y| y.as_str()).collect();
+            let first = numbers.first().unwrap();
+            let last = numbers.last().unwrap();
+            let concat_number = "".to_owned() + first + last;
+            let parsed_number: u32 = concat_number.parse::<u32>().ok().unwrap();
+            parsed_number
+        }
+    ).collect();
+    // for each line
+    // filter all non numbers
+    // get first and last number
+    // create a number
+    // Sum all numbers
+    let summed_number: u32 = numbers_collected.iter().sum();
+    Some(summed_number)
 }
 
 #[cfg(test)]
